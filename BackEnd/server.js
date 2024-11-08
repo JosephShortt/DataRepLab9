22c83781531fcb55a985b7e24d8a913c627af6bd
JosephShortt <josephshortt1@gmail.com>
2024-11-08 10:30:56 +0000

    Completed final exercise

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -12,31 +12,42 @@ app.use(function(req, res, next) {
   next();
 });
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get('/api/movies', (req, res) => {
     const movies = [
         {
-            "Title": "Avengers: Infinity War",
+            "Title": "Avengers: Infinity War (server)",
             "Year": "2018",
             "imdbID": "tt4154756",
             "Type": "movie",
-            "Poster": "https://example.com/poster1.jpg"
-        },
-        {
-            "Title": "Captain America: Civil War",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+          },
+          {
+            "Title": "Captain America: Civil War (server)",
             "Year": "2016",
             "imdbID": "tt3498820",
             "Type": "movie",
-            "Poster": "https://example.com/poster2.jpg"
-        },
-        {
-            "Title": "World War Z",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+          },
+          {
+            "Title": "World War Z (server)",
             "Year": "2013",
             "imdbID": "tt0816711",
             "Type": "movie",
-            "Poster": "https://example.com/poster3.jpg"
-        }
+            "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+          }
     ];
     res.json({ movies });
+});
+
+app.post('/api/movies', (req,res)=>{
+    
+    console.log("Movie: "+req.body.Title);
+    res.send("Movies received")
+
 });
 
 app.listen(port, () => {
--- src/components/Create.js
+++ src/components/Create.js
@@ -1,6 +1,7 @@
 // create.js
 //Import useState
 import { useState } from "react";
+import axios from "axios";
 
 function Create() {
   // Define state variables to manage form inputs for movie title, year, and poster URL
@@ -11,43 +12,55 @@ function Create() {
   // Function to handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title,year,poster);
-    
+
+    console.log(`Title: ${title}, Year: ${year}, Poster: ${poster}`);
+
+    const movie = {
+      title: title,
+      year: year,
+      poster: poster
+    };
+
+    axios.post('http://localhost:4000/api/movies', movie)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err.data));
+
   }
+
   //Returns each entered input into console for movie title, year and poster
   return (
-     <div>
-    <h2>This is my Create Component.</h2>
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label>Add Movie Title: </label>
-        <input type="text"
-          className="form-control"
-          value={title}
-          onChange={(e) => { setTitle(e.target.value) }}
-        />
-      </div>
+    <div>
+      <h2>This is my Create Component.</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Add Movie Title: </label>
+          <input type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }}
+          />
+        </div>
 
-      <div className="form-group">
-        <label>Add Movie Year: </label>
-        <input type="text"
-          className="form-control"
-          value={year}
-          onChange={(e) => { setYear(e.target.value) }}
-        />
-      </div>
+        <div className="form-group">
+          <label>Add Movie Year: </label>
+          <input type="text"
+            className="form-control"
+            value={year}
+            onChange={(e) => { setYear(e.target.value) }}
+          />
+        </div>
 
-      <div className="form-group">
-        <label>Add Movie Poster URL: </label>
-        <input type="text"
-          className="form-control"
-          value={poster}
-          onChange={(e) => { setPoster(e.target.value) }}
-        />
-      </div>
-      <input type="submit" value="Add Movie" />
-    </form>
-  </div>
+        <div className="form-group">
+          <label>Add Movie Poster URL: </label>
+          <input type="text"
+            className="form-control"
+            value={poster}
+            onChange={(e) => { setPoster(e.target.value) }}
+          />
+        </div>
+        <input type="submit" value="Add Movie" />
+      </form>
+    </div>
   );
 }
 
