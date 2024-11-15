dd033fa60aafb155f5c9d86a8aff35ece64a59a5
JosephShortt <josephshortt1@gmail.com>
2024-11-15 10:00:17 +0000

    Added movie to db

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 app.use(cors()); // Allow cross-origin requests
 
 //handle CORS headers manually
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -23,43 +23,54 @@ app.use(bodyParser.json()); // Enable JSON data parsing
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://admin:admin@cluster0.y6xzc.mongodb.net/MyMovieDB');
 
+const movieSchema = new mongoose.Schema({
+  title: String,
+  year: String,
+  poster: String
+});
+
+const MovieModel = new mongoose.model('myMovies', movieSchema);
+
 // Define a GET endpoint to return a list of movies
 app.get('/api/movies', (req, res) => {
-    const movies = [ // Array of sample movies to be returned
-      
-        {
-            "Title": "Avengers: Infinity War (server)",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-          },
-          {
-            "Title": "Captain America: Civil War (server)",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-          },
-          {
-            "Title": "World War Z (server)",
-            "Year": "2013",
-            "imdbID": "tt0816711",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-          }
-    ];
-    res.json({ movies }); // Send the movies array as JSON response
+  const movies = [ // Array of sample movies to be returned
+
+    {
+      "Title": "Avengers: Infinity War (server)",
+      "Year": "2018",
+      "imdbID": "tt4154756",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+    },
+    {
+      "Title": "Captain America: Civil War (server)",
+      "Year": "2016",
+      "imdbID": "tt3498820",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+    },
+    {
+      "Title": "World War Z (server)",
+      "Year": "2013",
+      "imdbID": "tt0816711",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+    }
+  ];
+  res.json({ movies }); // Send the movies array as JSON response
 });
 
 // Define a POST endpoint to receive and log a new movie
-app.post('/api/movies', (req,res)=>{
-    
-    console.log("Movie: "+req.body.Title);
-    res.send("Movies received") // Send a response indicating the movie was received
+app.post('/api/movies', async (req, res) => {
 
+  console.log("Movie: " + req.body.Title);
+
+  const { title, year, poster } = req.body;
+  const newMovie  = new MovieModel({ title, year, poster });
+  await newMovie.save();
+  res.status(201).json({ message: 'Movie created successfully', movie: newMovie });
 });
 // Start the server and listen on the specified port
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
