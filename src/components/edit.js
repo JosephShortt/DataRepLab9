5ab19f40921a81459d1227efc3344b117f152ad1
JosephShortt <josephshortt1@gmail.com>
2024-11-24 13:54:47 +0000

    commented

--- src/components/edit.js
+++ src/components/edit.js
@@ -5,15 +5,19 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 
+// Functional component for editing a movie
 export default function Edit(props) {
   let { id } = useParams();
   const [title, setTitle] = useState("");
   const [year, setYear] = useState("");
   const [poster, setPoster] = useState("");
+  // Hook to navigate to a different route
   const navigate = useNavigate();
 
+// Fetch movie details when the component is mounted or when the `id` changes
 useEffect(() => {
     axios.get('http://localhost:4000/api/movie/' + id)
+        // Set the state with the retrieved movie details
         .then((response) => {
             setTitle(response.data.title);
             setYear(response.data.year);
@@ -24,9 +28,13 @@ useEffect(() => {
         });
 }, [id]);
 
+  // Handle the form submission to edit the movie
 const handleSubmit = (event) => {
     event.preventDefault();
+    //Create a new movie with updated details
     const newMovie = { id, title, year, poster };
+
+    // Make a PUT request to update the movie details
     axios.put('http://localhost:4000/api/movie/' + id, newMovie)
         .then((res) => {
             console.log(res.data);
@@ -34,6 +42,7 @@ const handleSubmit = (event) => {
         });
 }
 
+  // Render the form to edit movie details
 return (
     <div>
         <form onSubmit={handleSubmit}>
